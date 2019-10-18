b363264bf0cd19fa1bcd9da1ffd35f06acbff9c6
YonatanRA <yeho@mail.com>
2019-10-17 19:27:12 -0500

    keras utils

--- Keras-Tensorflow/keras_utils.py
+++ Keras-Tensorflow/keras_utils.py
@@ -65,15 +65,15 @@ class ModelSaveCallback(keras.callbacks.Callback):
 
 # !!! remember to clear session/graph if you rebuild your graph to avoid out-of-memory errors !!!
 def reset_tf_session():
-    curr_session = tf.get_default_session()
+    curr_session = tf.compat.v1.get_default_session()
     # close current session
     if curr_session is not None:
         curr_session.close()
     # reset graph
     K.clear_session()
     # create new session
-    config = tf.ConfigProto()
+    config = tf.compat.v1.ConfigProto()
     config.gpu_options.allow_growth = True
-    s = tf.InteractiveSession(config=config)
+    s = tf.compat.v1.InteractiveSession(config=config)
     K.set_session(s)
     return s
